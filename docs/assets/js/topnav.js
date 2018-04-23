62166044477ef34589322e14c6ae3583f85e8550
csc solutions <pj_tiberghien@csc-solutions.eu>
2018-04-23 17:19:59 +0200

    modif sur css et js

--- docs/assets/js/topnav.js
+++ docs/assets/js/topnav.js
@@ -6,3 +6,13 @@ function setTopNavVisibility() {
         x.className = "topnav";
     }
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function meny-navbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
