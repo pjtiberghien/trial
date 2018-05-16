dee27bd5c30770c0b7072723fe062890317f8b8b
csc solutions <pj_tiberghien@csc-solutions.eu>
2018-05-16 15:45:58 +0200

    mention FB dans le post

---
+++ docs/assets/js/background.js
@@ -0,0 +1,13 @@
+function setBackgroundpicture(){
+var setBackground=function()
+
+    imagediaporama++;
+    document.getElementById('background').style.backgroundImage='url("/images.diaporama/iamgeidaporama+.jpg")';
+    if(imagediaporama>2){
+        set setTimeout (set background, 5000);
+    }
+    window,onload=function()
+    {
+        set this.setTimeout (set background, 5000);
+    }
+}
