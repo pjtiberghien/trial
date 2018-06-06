278aa779943f3729d5013c7a9a2f08053e96f88a
csc solutions <pj_tiberghien@csc-solutions.eu>
2018-06-06 15:50:25 +0200

    Copy of staticsite

--- docs/assets/js/topnav.js
+++ docs/assets/js/topnav.js
@@ -1,15 +1,5 @@
 function setTopNavVisibility() {
-    var x = document.getElementById("mytopnav2");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function menu_navbar() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("mytopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
